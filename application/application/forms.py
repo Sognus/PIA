a2e6f91cea92d1db33fc138a677a6497ca214b3b
Jakub Vítek <sognus@sognus.cz>
2021-01-07 06:00:01 +0100

    Chat frontend

--- application/application/forms.py
+++ application/application/forms.py
@@ -8,23 +8,27 @@ class LoginForm(forms.Form):
                                                                               'placeholder': 'Email'}))
     password = forms.CharField(label="Heslo", widget=forms.PasswordInput(attrs={'class': 'form-control',
                                                                                 'placeholder': 'Heslo'}))
+    hidden = forms.CharField(label="Hidden", widget=forms.HiddenInput, required=False)
 
     def clean(self):
         # Get data
-        data_username = self.cleaned_data["username"]
-        data_password = self.cleaned_data["password"]
+        data_username = self.cleaned_data.get("username", None)
+        data_password = self.cleaned_data.get("password", None)
 
-        user = None
+        if data_password is None or data_password is None:
+            self.add_error("hidden", "Formulář nebyl vyplněn!")
+        else:
+            user = None
 
-        # Check user exist
-        try:
-            user = User.objects.get(email=data_username)
+            # Check user exist
+            try:
+                user = User.objects.get(email=data_username)
 
-            if not check_password(data_password, user.password):
-                self.add_error("password", "Zadejte správné heslo!")
+                if not check_password(data_password, user.password):
+                    self.add_error("password", "Zadejte správné heslo!")
 
-        except User.DoesNotExist:
-            self.add_error("username", "Zadaný uživatel neexistuje!")
+            except User.DoesNotExist:
+                self.add_error("username", "Zadaný uživatel neexistuje!")
 
         # Return data
         return self.cleaned_data
@@ -32,6 +36,35 @@ class LoginForm(forms.Form):
 
 class RegisterForm(forms.Form):
     username = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={'class': 'form-control',
-                                                                              'placeholder': 'Email'}))
+                                                                              'placeholder': 'Email',
+                                                                              "id": "username"}))
     password = forms.CharField(label="Heslo", widget=forms.PasswordInput(attrs={'class': 'form-control',
                                                                                 'placeholder': 'Heslo'}))
+    password2 = forms.CharField(label="Heslo2", widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                                  'placeholder': 'Heslo znovu'}))
+    hidden = forms.CharField(label="Hidden", widget=forms.HiddenInput, required=False)
+
+    def clean(self):
+        # Get data
+        data_username = self.cleaned_data.get("username", None)
+        data_password = self.cleaned_data.get("password", None)
+        data_password2 = self.cleaned_data.get("password2", None)
+
+        if data_password is None or data_password is None or data_password2 is None:
+            self.add_error("hidden", "Formulář nebyl vyplněn!")
+            return self.cleaned_data
+        else:
+            # Check if user exists
+            try:
+                user = User.objects.get(email=data_username)
+            except User.DoesNotExist:
+                user = None
+
+            if user is not None:
+                self.add_error("username", "Zadaný e-mail byl již registrován!")
+
+            # Check password match
+            if data_password != data_password2:
+                self.add_error("password2", "Hesla se neshodují!")
+
+        return self.cleaned_data
--- application/application/views.py
+++ application/application/views.py
@@ -7,22 +7,25 @@ from . import forms
 
 
 def index(request):
-    # Show login page if user is not logged
     title = "Přihlášení"
     if not request.user.is_authenticated:
         if request.method == "POST":
+            # Form validation
             form = forms.LoginForm(request.POST)
             if form.is_valid():
+                # User login
                 user = User.objects.get(email=form.cleaned_data["username"])
                 login(request, user)
                 return HttpResponseRedirect("/lobby")
             else:
+                # Send forms with errors
                 return render(request, "application/login.html", {"form": form, "title": title})
         else:
+            # Send form
             form = forms.LoginForm()
             return render(request, "application/login.html", {"form": form, "title": title})
     else:
-        # Redirect to lobby
+        # Redirect to lobby when user is logged
         return HttpResponseRedirect("/lobby")
 
 
@@ -30,10 +33,23 @@ def register(request):
     # Show login page if user is not logged
     title = "Registrace"
     if not request.user.is_authenticated:
-        form = forms.LoginForm()
-        return render(request, "application/register.html", {"form": form, "title": title})
+        if request.method == "POST":
+            form = forms.RegisterForm(request.POST)
+            if form.is_valid():
+                # Register User
+                user = User.objects.create_user(form.cleaned_data["username"], form.cleaned_data["username"], form.cleaned_data["password"])
+                # Login User
+                login(request, user)
+                # Redirect User to lobby
+                return HttpResponseRedirect("/lobby")
+            else:
+                return render(request, "application/register.html", {"form": form, "title": title})
+        else:
+            # Send form
+            form = forms.RegisterForm()
+            return render(request, "application/register.html", {"form": form, "title": title})
     else:
-        # Redirect to lobby
+        # Redirect to lobby when user is logged
         return HttpResponseRedirect("/lobby")
 
 
@@ -42,4 +58,3 @@ def log_out(request):
     logout(request)
     # Redirect him to login page
     return HttpResponseRedirect('/login')
-
